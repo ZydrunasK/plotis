bccd0f3a157775c090531cdb7bc7aa91fa9b945c
Zydrunask <kniukstaz@gmail.com>
2024-09-18 13:25:17 +0300

    main basic start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './components/header/Header';
+import { Main } from './components/main/Main';
 
 export function App() {
 
   return (
     <>
     <Header />
+    <Main />
     </>
   )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,25 @@
-// eslint-disable-next-line no-unused-vars
 import s from './main.module.css';
 
 export function Main() {
     return (
-        <>
-        </>
+        <main className={s.main} >
+            <section className={s.container}>
+                <div className={s.plotis}>
+                    <p>Plotis</p>
+                    <button>+</button>
+                    <span></span>
+                    <button>-</button>
+                </div>
+                <div className={s.aukstis}>
+                    <p>Aukštis</p>
+                    <button>+</button>
+                    <span></span>
+                    <button>-</button>
+                </div>
+                <div className={s.lenta}>
+        
+                </div>
+            </section>
+        </main>
     )
 }
